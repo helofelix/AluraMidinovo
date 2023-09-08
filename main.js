98a200dcc1ea430eee7e7dbd375badc2ff23b764
helofelix <107410996+helofelix@users.noreply.github.com>
2023-09-08 10:57:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-document.querySelector('.tecla_pom')
+function tocaSomPom(){
+document.querySelector('#som_tecla_pom').play(); 
+}
 
-document.querySelector('#som_tecla_pom').play();
+ document.querySelectorAll('.tecla');
+ const listaDeTeclas = document.querySelectorAll('.tecla');
+    
+    
